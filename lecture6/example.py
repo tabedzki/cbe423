a788dbbabc0b7d6b6eb75e236f76f94022957ed5
Andrew Rosen <asrosen93@gmail.com>
2026-01-15 22:06:31 -0500

    Update

--- lecture6/example.py
+++ lecture6/example.py
@@ -2,10 +2,10 @@ from ase.build import bulk
 from ase.calculators.vasp import Vasp
 
 atoms = bulk("Fe")
+atoms.set_initial_magnetic_moments([5.0] * len(atoms))
 
 # This is a minimal example and very inaccurate!
 # In practice, you would need to set many more VASP parameters.
 # Again, this is not a "real" calculation!
 atoms.calc = Vasp(xc="PBE")
-atoms.set_initial_magnetic_moments([5.0] * len(atoms))
 atoms.get_potential_energy()
